1938ee7b43a94ab4ddb0c4bf0886b78d64197129
Malik Harrison <harrmalik@gmail.com>
2017-01-24 22:12:59 -0500

    now displays each summoners divison and tier

---
+++ React/App.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Row from './components/Row'
+
+var App = React.createClass({
+    getInitialState() {
+        return {
+            data: []
+        }
+    },
+    componentDidMount() {
+        this.getRankings()
+    },
+    getRankings() {
+        let component = this
+
+        $.get('/api/getRankings', function(data) {
+            component.setState({data})
+        })
+    },
+    eachSummoner(summoner) {
+         return (
+             <Row
+                key={summoner.name}
+                name={summoner.name}
+                tier={summoner.tier}
+                division={summoner.division}
+                leaguePoints={summoner.leaguePoints} />
+        )
+    },
+    render() {
+        return (
+            <div className="main">
+                <section className="ui container">
+                <table className="ui very basic collapsing celled table">
+                  <thead>
+                    <tr><th>Summoner</th>
+                    <th>League Points</th>
+                  </tr></thead>
+                  <tbody>
+                    {this.state.data.map(this.eachSummoner)}
+                  </tbody>
+                </table>
+
+                </section>
+
+                <footer>
+
+                </footer>
+            </div>
+        )
+    }
+})
+
+ReactDom.render(<App />,
+document.getElementById('react-app'))
---
+++ React/components/Row.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+var Row = React.createClass({
+    getInitialState() {
+        return {
+
+        }
+    },
+    render() {
+        let summoner = this.props
+        return (
+            <tr>
+              <td>
+                <h4 className="ui image header">
+                  <img src="/images/avatar2/small/lena.png" className="ui mini rounded image"></img>
+                  <div className="content">
+                    {summoner.name}
+                    <div className="sub header">{summoner.tier + ' ' + summoner.division}
+                  </div>
+                </div>
+              </h4></td>
+              <td>
+                {summoner.leaguePoints}
+              </td>
+            </tr>
+        )
+    }
+});
+
+export default Row
